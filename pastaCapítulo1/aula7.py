3dd9bbf3ea8f35b120677abd57b0b9ac9ae9c6a5
Pedro Henrique de Souza e Silva <desouzaesilvap@gmail.com>
2023-12-13 18:10:17 -0300

    Indo para o primeiro exercício

---
+++ pastaCapítulo1/aula7.py
@@ -0,0 +1,7 @@
+soma_dois_mais_dois = 2 + 2
+nome_completo = 'Pedro Henrique de Souza e Silva'
+
+nome = 'Pedro'
+idade = 19
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade, 'Se é maior de idade:', maior_de_idade)
