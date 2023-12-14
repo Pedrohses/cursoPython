670dbc9695f5a682e9253965bb806e968186854c
Pedro Henrique de Souza e Silva <desouzaesilvap@gmail.com>
2023-12-14 20:32:34 -0300

    Indo para o exercício

---
+++ aula26.py
@@ -0,0 +1,8 @@
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{-2345.6792456:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08x}')
+print(f'{variavel!r}')
---
+++ aula27.py
@@ -0,0 +1,5 @@
+variavel  = 'Olá mundo'
+print(variavel[-4])
+print(variavel[0:9:2])
+print(variavel[::-1])
+print(len(variavel))
