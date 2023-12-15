3eb9e4af4743148c2899ab5d844777a57ad7e30b
Pedro Henrique de Souza e Silva <desouzaesilvap@gmail.com>
2023-12-14 21:14:35 -0300

    Exercício finalizado

---
+++ pastaCapítulo1/aula28.py
@@ -0,0 +1,17 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+espaço = ' '
+
+if nome != '' and idade != '':
+    int_idade = int(idade)
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    if espaço in nome:
+        print('Seu nome contém espaços')
+    else:
+        print('Seu nome não contém espaço')
+    print(f'Seu nome contém {len(nome)} letras')
+    print(f'A primeira letra do seu nome é {nome[0:1]}')
+    print(f'A ultima letra do seu nome é {nome[-1]}')
+else:
+    print('Desculpe, você deixou campos vazios')
