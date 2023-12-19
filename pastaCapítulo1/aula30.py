69eaefc03f027f5c6eedeaa296dc866e65bed8ed
Pedro Henrique de Souza e Silva <desouzaesilvap@gmail.com>
2023-12-19 20:18:40 -0300

    Indo jantar

---
+++ pastaCapítulo1/aula30.py
@@ -0,0 +1,11 @@
+velocidade = 61
+local_carro = 90
+
+RADAR_1 = 60
+LOCAL_1 = 100
+RADAR_RANGE = 1
+
+if velocidade > RADAR_1 and local_carro == LOCAL_1:
+    print('MULTADO!')
+else:
+    print('NÃO MULTADO')
