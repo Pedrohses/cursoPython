360f19e5642300d4ff17dd97af21445e818ce4ec
Pedro Henrique de Souza e Silva <desouzaesilvap@gmail.com>
2023-12-29 15:07:51 -0300

    Adicionando comentários

--- pastaCapítulo1/tentando_fazer_exercicio_aula42_sozinho.py
+++ pastaCapítulo1/tentando_fazer_exercicio_aula42_sozinho.py
@@ -10,10 +10,12 @@ while i < len(frase):
     letra_atual = frase[i]
     qntd_atual = frase.count(letra_atual)
 
+    # Só para pular os espaços
     if ' ' in letra_atual:
         i += 1
         continue
     
+    #Para atribuir o valor da letra que aparece mais vezes e quantas vezes aparece para as variáveis certas
     if qntd_atual > qntd_mais_vezes:
         qntd_mais_vezes = qntd_atual
         letra_que_apareceu_mais_vezes = letra_atual
