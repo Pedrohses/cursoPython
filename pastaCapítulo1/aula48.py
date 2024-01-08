7e0f05921e9da3d4fc2098cd87875a999ee8fc3f
Pedro Henrique de Souza e Silva <desouzaesilvap@gmail.com>
2024-01-08 18:19:17 -0300

    Aula 48

---
+++ pastaCapítulo1/aula48.py
@@ -0,0 +1,11 @@
+string = 'ABCDE'
+# lista = list()
+
+# print(bool([])) # false
+# print(lista, type(lista))
+
+#.........0.....1..........2...........3....4
+#........-5....-4.........-3..........-2...-1
+lista = [123, True, 'Pedro Henrique', 1.2, []] 
+lista[-3] = 'Maria'
+print(lista[2], type(lista[2]))
