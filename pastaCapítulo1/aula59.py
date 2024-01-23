58f812805defea3b8068a3de94d1e0b06669997f
Pedro Henrique de Souza e Silva <desouzaesilvap@gmail.com>
2024-01-23 18:16:32 -0300

    deixando as aulas que eu tinha adiantado em dia

---
+++ pastaCapítulo1/aula59.py
@@ -0,0 +1,22 @@
+string = 'ABCD'
+lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+tupla = "Python", 'é', 'legal'
+
+salas = [
+    ['Maria', 'Helena'],
+
+    ['Elaine', ],
+
+    ['Luiz', 'João', 'Eduarda'],
+]
+
+
+# p, b, *_, ap, u = lista
+# print(p, u, ap)
+
+# for nome in lista:
+#     print(nome, end=' ')
+# print(*lista)
+# print(*string)
+
+print(*salas, end='\n')
